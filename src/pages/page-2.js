bcadca205c216d80c059ba9a069de76e6a29591c
Krishnaaaaaaaaaaaa <96303773+Krishnaaaaaaaaaaaa@users.noreply.github.com>
2021-12-17 20:56:00 +0530

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,8 +7,8 @@ import Seo from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <Seo title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>Hi from the second page of businessssss</h1>
+    <p>Welcome to page  ok2</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
